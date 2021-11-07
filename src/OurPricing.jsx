6625cdd736be0259448319e74f7d5a34332928a5
Joel S. Almeida <joel.almeida.sa@gmail.com>
2021-11-07 14:35:09 -0300

    Feature/Refactoring: Componente "OurPricing" fica responsavel por alternar planos entre anual e mensal. "Cards" recebem tipo de plano via props do componente "OurPricing" e alteram os preços de acordo com esta condiçao. :)

--- src/OurPricing.jsx
+++ src/OurPricing.jsx
@@ -2,6 +2,28 @@ import React from 'react';
 import PlanCard from './PlanCard';
 
 class OurPricing extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      planType: 'monthly',
+    };
+    this.togglePlanType = this.togglePlanType.bind(this);
+  }
+  
+  togglePlanType() {
+    const { planType } = this.state;
+
+    if (planType === 'monthly') {
+      this.setState({
+        planType: 'anual'
+      });
+    } else {
+      this.setState({
+        planType: 'monthly'
+      });
+    }
+  }
+
   render() {
     return (
       <section className="our-pricing">
@@ -9,7 +31,7 @@ class OurPricing extends React.Component {
 
         <div className="our-pricing__button-container">
           Annually
-          <div className="our-pricing__button">
+          <div className="our-pricing__button" onClick={this.togglePlanType}>
             <span className="our-pricing__button__toggle"></span>
           </div>
           Monthly
@@ -18,26 +40,32 @@ class OurPricing extends React.Component {
         <div className="our-pricing__plans">
           <PlanCard
             type="Basic"
-            price="19.99"
+            monthlyPrice="19.99"
+            anualPrice="199.99"
             descriptionOne="500 GB Storage"
             descriptionTwo="2 Users Allowed"
             descriptionThree="Send up to 3 GB"
+            planType={ this.state.planType }
           />
 
           <PlanCard
             type="Professional"
-            price="24.99"
+            monthlyPrice="24.99"
+            anualPrice="249.99"
             descriptionOne="1 TB Storage"
             descriptionTwo="5 Users Allowed"
             descriptionThree="Send up to 10 GB"
+            planType={ this.state.planType }
           />
 
           <PlanCard
             type="Master"
-            price="39.99"
+            monthlyPrice="39.99"
+            anualPrice="399.99"
             descriptionOne="2 TB Storage"
             descriptionTwo="10 Users Allowed"
             descriptionThree="Send up to 20 GB"
+            planType={ this.state.planType }
           />
         </div>
       </section>
--- src/PlanCard.jsx
+++ src/PlanCard.jsx
@@ -1,47 +1,27 @@
 import React from 'react';
 
 class PlanCard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      monthly: true,
-      type: this.props.type,
-      price: this.props.price,
-      descriptionOne: this.props.descriptionOne,
-      descriptionTwo: this.props.descriptionTwo,
-      descriptionThree: this.props.descriptionThree,
-    };
-    this.togglePrice = this.togglePrice.bind(this);
+  priceToShow(selectedPlan) {
+    if (selectedPlan === 'monthly') {
+      return this.props.monthlyPrice;
+    }  return this.props.anualPrice;
   }
-
-  togglePrice() {
-    if (this.state.monthly) {
-      this.setState((prevState) => ({
-        price: (prevState.price * 10).toFixed(2),
-        monthly: false,
-      }));
-    } else {
-      this.setState((prevState) => ({
-        price: (prevState.price / 10).toFixed(2),
-        monthly: true,
-      }));
-    }
-  }
-
+  
   render() {
+    const { planType } = this.props;
     return (
       <div className="plan-card">
-        <div className="plan-card__type">{this.state.type}</div>
+        <div className="plan-card__type">{this.props.type}</div>
         <div className="plan-card__price">
           <span className="price-span">$</span>
-          {this.state.price}
+          {this.priceToShow(planType)}
         </div>
         <div className="plan-card__description-container">
-          <div className="plan-card__description"> {this.state.descriptionOne} </div>
-          <div className="plan-card__description">{this.state.descriptionTwo} </div>
-          <div className="plan-card__description"> {this.state.descriptionThree}</div>
+          <div className="plan-card__description"> {this.props.descriptionOne} </div>
+          <div className="plan-card__description">{this.props.descriptionTwo} </div>
+          <div className="plan-card__description"> {this.props.descriptionThree}</div>
         </div>
-        <div className="hollow-button" onClick={this.togglePrice}>
+        <div className="hollow-button">
           Learn More
         </div>
       </div>
